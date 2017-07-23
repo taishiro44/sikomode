f9f50ba9d112d66f8095c6f2f63dc28670331f29
tanakataishiro <tanakataishiro@172.16.203.54>
2017-07-23 15:03:02 +0900

    音の数、音域を計算する処理を関数に分けました。

--- src/sikomode/Modeling.java
+++ src/sikomode/Modeling.java
@@ -46,118 +46,26 @@ public class Modeling {
     public void culcModel() {
         //差分を計算し、差分のあるTickのリストを取得
         List<Long> tickList = this.calcDiffece();
-        byte[] soundScore;
-        int[] soundRange = new int[tickList.size()];
-        int[] soundNum = new int[tickList.size()];
-        Arrays.fill(soundRange, 0);
-        Arrays.fill(soundNum, 0);
-        //ミスの直前の値
-        int[] preRange = new int[tickList.size()];
-        int[] preNum = new int[tickList.size()];
-        Arrays.fill(preRange, 0);
-        Arrays.fill(preNum, 0);
-        //ミスを含め、直後の値
-        int[] postRange = new int[tickList.size()];
-        int[] postNum = new int[tickList.size()];
-        Arrays.fill(postRange, 0);
-        Arrays.fill(postNum, 0);
+        //特徴
+        SoundFeature feature = new SoundFeature(tickList.size());
+        //ミスの直前、以後の値
+        SoundFeature pre;
+        SoundFeature post;
         //タイミング解像度は４分音符の長さなので、2倍することで２分音符分の長さをみる
-        //つまり、前後合わせて1小節の長さとなる
-        long tickRange = this.score.getResolution() * 2;
-        //tickRangeで指定した時間の間の音の総和
-        int[] soundSum = new int[128];
-        int[] preSum = new int[128];
-        int[] postSum = new int[128];
-        int minIdx = Integer.MAX_VALUE;
-        int maxIdx = Integer.MIN_VALUE;
-        long tick;
-        //ミスしたtickのリストから、ミスした箇所の特徴を抽出する
-        for (int i = 0; i < tickList.size(); i++) {
-            //0埋めで初期化
-            Arrays.fill(soundSum, (byte) 0);
-            Arrays.fill(preSum, (byte) 0);
-            Arrays.fill(postSum, (byte) 0);
-            //指定したtickの前後を見る。
-            tick = tickList.get(i);
-            for (long range = (tick - tickRange);
-                    range < (tick + tickRange); range++) {
-                //ファイルの先頭以前の部分ならばcontinue
-                if (range < 0) {
-                    continue;
-                }
-                //NoteOnを取得する
-                soundScore = this.score.getNoteOn(range);
-                //ファイル末尾ならば終了
-                if (soundScore[0] == -1) {
-                    break;
-                }
-                //音の総和をとる
-                for (int j = 0; j < soundScore.length; j++) {
-                    if(range < tick){ //ミスの直前まで
-                        preSum[j] += soundScore[j];
-                    }else{ //ミスの以後
-                        postSum[j] += soundScore[j];
-                    }
-                }
-            }
-            minIdx = Integer.MAX_VALUE;
-            maxIdx = Integer.MIN_VALUE;
-            for (int j = 0; j < preSum.length; j++) {
-                if (preSum[j] != 0) {
-                    if (minIdx > j) {
-                        minIdx = j;
-                    }
-                    if (maxIdx < j) {
-                        maxIdx = j;
-                    }
-                }
-            }
-            //差分の結果、抽出したtickの前後一小節の音域
-            preRange[i] = (maxIdx - minIdx);
-            minIdx = Integer.MAX_VALUE;
-            maxIdx = Integer.MIN_VALUE;
-            for (int j = 0; j < postSum.length; j++) {
-                if (postSum[j] != 0) {
-                    if (minIdx > j) {
-                        minIdx = j;
-                    }
-                    if (maxIdx < j) {
-                        maxIdx = j;
-                    }
-                }
-            }
-            //差分の結果、抽出したtickの前後一小節の音域
-            postRange[i] = (maxIdx - minIdx);
-            //音の数
-            for (int j = 0; j < preSum.length; j++) {
-                preNum[i] += preSum[j];
-            }
-            //音の数
-            for (int j = 0; j < postSum.length; j++) {
-                postNum[i] += postSum[j];
-            }
+        //４分の４拍子のため前後合わせて1小節の長さとなる
+        int tickRange = this.score.getResolution()* 2;
+        //特徴抽出
+        pre = this.extractFeature(tickList, tickRange, 0);
+        post = this.extractFeature(tickList, 0, tickRange);
+        //前後の変化量を計算
+        for(int i = 0; i < pre.range.length;i++){
+            feature.range[i] = post.range[i] - pre.range[i];
         }
-        for(int i = 0; i < preRange.length;i++){
-            soundRange[i] = postRange[i] - preRange[i];
-        }
-        for(int i = 0; i < preNum.length;i++){
-            soundNum[i] = postNum[i] - preNum[i];
+        for(int i = 0; i < pre.num.length;i++){
+            feature.num[i] = post.num[i] - pre.num[i];
         }
         //結果をファイルに保存
-        this.save2File(soundRange, soundNum);
-
-        /*DEBUG*/
-        double rangeAve = 0;
-        double numAve = 0;
-        for (int i = 0; i < soundRange.length; i++) {
-            rangeAve += soundRange[i];
-            numAve += soundNum[i];
-        }
-        rangeAve /= soundRange.length;
-        numAve /= soundNum.length;
-        System.out.println("平均");
-        System.out.println("幅 : " + rangeAve + ", 数 : " + numAve);
-        /*DEBUG*/
+        this.save2File(feature.range, feature.num);
     }
 
     /**
@@ -204,6 +112,70 @@ public class Modeling {
         return tickList;
     }
 
+    /**
+     * 特徴を抽出する。<br>
+     * 抽出する特徴は、ミス前後の音の数と音域の変化量とする。<br>
+     * preRange <= tickList.get(index) <= postRangeの範囲を探索する。<br>
+     * @param tickList tickのリスト
+     * @param preRange 指定したtickの前の範囲
+     * @param postRange 指定したtickの後の範囲
+     * @return 
+     */
+    private SoundFeature extractFeature(List<Long> tickList, 
+            int preRange, int postRange){
+        //取得したNoteOnを格納する
+        byte[] soundScore;
+        //tickRangeで指定した時間の間の音の総和
+        int[] soundSum = new int[128];
+        //指定するtick
+        long tick;
+        SoundFeature feature = new SoundFeature(tickList.size());
+        int minIdx, maxIdx;
+        //ミスした箇所のtickのリストから、ミスした箇所の特徴を抽出する
+        for (int i = 0; i < tickList.size(); i++) {
+            Arrays.fill(soundSum, (byte) 0);
+            //指定したtickの前後を見る。
+            tick = tickList.get(i);
+            for (long range = (tick - preRange);
+                    range < (tick + postRange); range++) {
+                //ファイルの先頭以前の部分ならばcontinue
+                if (range < 0) {
+                    continue;
+                }
+                //NoteOnを取得する
+                soundScore = this.score.getNoteOn(range);
+                //ファイル末尾ならば終了
+                if (soundScore[0] == -1) {
+                    break;
+                }
+                //音の総和をとる
+                for (int j = 0; j < soundScore.length; j++) {
+                    soundSum[j] += soundScore[j];
+                }
+            }
+            //音域をとる
+            minIdx = Integer.MAX_VALUE;
+            maxIdx = Integer.MIN_VALUE;
+            for (int j = 0; j < soundSum.length; j++) {
+                if (soundSum[j] != 0) {
+                    if (minIdx > j) {
+                        minIdx = j;
+                    }
+                    if (maxIdx < j) {
+                        maxIdx = j;
+                    }
+                }
+            }
+            //差分の結果、抽出したtickの前後一小節の音域
+            feature.range[i] = (maxIdx - minIdx);
+            //音の数
+            for (int j = 0; j < soundSum.length; j++) {
+                feature.num[i] += soundSum[j];
+            }
+        }
+        return feature;
+    }
+    
     /**
      * x, y の二次元の情報をファイルに保存します。<br>
      * x, y は空白区切りで保存されます。<br>
---
+++ src/sikomode/SoundFeature.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sikomode;
+
+import java.util.Arrays;
+
+/**
+ * 音の特徴を格納するクラス
+ * @author tanakataishiro
+ */
+public class SoundFeature {
+    public int [] num;
+    public int [] range;
+    public long [] tick;
+
+    public SoundFeature(int size) {
+        //指定されたサイズで初期化 & 0埋め
+        this.num = new int[size];
+        this.range = new int[size];
+        this.tick = new long[size];
+        Arrays.fill(this.num, 0);
+        Arrays.fill(this.range, 0);
+        Arrays.fill(this.tick, 0);
+    }
+    
+}
